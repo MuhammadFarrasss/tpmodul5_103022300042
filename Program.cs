c96b8b9eade074e8e2af5fcddef057c27a28943a
MuhammadFarrasss <muhammadfarraaas@gmail.com>
2025-03-16 14:44:38 +0700

    generic-method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class haloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program { 
+
+    static void Main()
+    {
+        haloGeneric halo = new haloGeneric();
+        halo.SapaUser("Farras");
+    }
+}
